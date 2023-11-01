11d6900a541f4112fd9c73df0530541eede15188
Yehor Smoliakov <yehors@ukr.net>
2023-11-01 13:58:16 +0000

    Clean up

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-	// "math/rand"
-	// "time"
 
 	log "github.com/sirupsen/logrus"
 )
-
-// const (
-// 	// upperBound is the upper bound of the random int range
-// 	upperBound = 101
-// )
-
-// func init() {
-// 	rand.Seed(time.Now().Unix())
-// }
 
 // Request is the function's request struct
 type Request struct {
